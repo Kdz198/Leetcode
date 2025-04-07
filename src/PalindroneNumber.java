bf199c3e79a407a06afff48e14279781270e91ae
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-07 18:10:22 +0700

    PalindroneNumber

---
+++ src/PalindroneNumber.java
@@ -0,0 +1,37 @@
+public class PalindroneNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello fpt");
+        xdx palindroneNumber123 = new xdx();
+        palindroneNumber123.isPalindrome(322345);
+    }
+}
+
+
+class xdx {
+    public boolean isPalindrome(int x) {
+        int flag = 0;
+        long temp = 0;
+        int z = x;
+        if (x < 0) {
+            return false;
+        }
+        if (x<10)
+            return true;
+        while (x>0) {
+            int y = x % 10;
+            x = x / 10;
+            if ( flag==0)
+            {
+                flag++;
+                temp = temp+y;
+            }
+            else
+            {
+                temp = temp*10+y;
+            }
+
+        }
+        return temp == z;
+    }
+}
