9d8b9abaa528f3c5dd65970e54a72582b02fc0f7
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-13 16:53:22 +0700

    SqrtX

---
+++ src/SqrtX.java
@@ -0,0 +1,28 @@
+public class SqrtX {
+    public static void main(String[] args) {
+
+        Sqrt123 sqrt = new Sqrt123();
+        System.out.println(sqrt.mySqrt(4));
+
+    }
+}
+
+class Sqrt123 {
+    public int mySqrt(int x) {
+
+        if (x == 0)
+            return 0;
+        if (x<4)
+            return 1;
+        if (x==5)
+            return 2;
+        for (long i = 2; i <= x / 2; i++) {
+            if (i*i == x)
+                return (int)i;
+            else if (i*i > x)
+                return (int)i-1;
+        }
+
+        return 0;
+    }
+}
