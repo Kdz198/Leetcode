6e45746a5ab3dabd5ccd4c79f7b91255c5e98ea1
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-13 14:11:57 +0700

    LengthOfLastWord

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,25 @@
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+
+        lengthOfLastWord123 lengthOfLastWord123 = new lengthOfLastWord123();
+        System.out.println(lengthOfLastWord123.lengthOfLastWord("   fly me   to   the moon  "));
+
+    }
+}
+//0ms beat 100%
+class lengthOfLastWord123 {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        char [] chars = s.toCharArray();
+        int count =0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (chars[i] == ' ') {
+                break;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
