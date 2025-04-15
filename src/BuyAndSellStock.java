44d0d8214617bf41ff201df84846d7f2ffc2f483
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-15 14:22:16 +0700

    BuyAndSellStock

---
+++ src/BuyAndSellStock.java
@@ -0,0 +1,25 @@
+public class BuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        BuyAndSellStock2 b = new BuyAndSellStock2();
+        System.out.println(b.maxProfit(prices));
+    }
+}
+
+class BuyAndSellStock2 {
+    public int maxProfit(int[] prices) {
+
+        int maxprofit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (maxprofit < prices[i] - min) {
+                maxprofit = prices[i] - min;
+            }
+        }
+
+        return maxprofit;
+    }
+}
