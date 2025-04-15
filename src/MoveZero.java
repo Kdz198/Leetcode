5ffae30f492388d5bc58db22078e82e15f1f8677
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-16 03:09:26 +0700

    MoveZero

---
+++ src/MoveZero.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MoveZero {
+    public static void main(String[] args) {
+        moveZero123 m = new moveZero123();
+        int [] nums = {0,1,0,2,3,4,5,6,7,8,9};
+        m.moveZeroes(nums);
+    }
+}
+class moveZero123 {
+    public void moveZeroes(int[] nums) {
+        ArrayList<Integer> list = new ArrayList();
+        int count =0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) list.add(nums[i]);
+            else
+                count++;
+        }
+        for (int i=0;i<count;i++)
+            list.add(0);
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = list.get(i);
+        }
+
+    }
+}
