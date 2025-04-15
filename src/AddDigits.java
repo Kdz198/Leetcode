7b8d4f4cd7beaf30096240009ee18496a2c7a7bc
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-16 01:34:13 +0700

    AddDigits

---
+++ src/AddDigits.java
@@ -0,0 +1,32 @@
+public class AddDigits {
+    public static void main(String[] args) {
+
+        addDigits123 a = new addDigits123();
+        System.out.println(a.addDigits(38));
+    }
+}
+
+class addDigits123 {
+    public int addDigits(int num) {
+
+        if (num < 10) {
+            return num;
+        }
+        int sum = 0;
+
+        while (true) {
+            while (num > 0) {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            if (sum <= 9)
+                break;
+            else {
+                num = sum;
+                sum = 0;
+            }
+        }
+
+        return sum;
+    }
+}
