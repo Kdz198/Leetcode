b41c0d7bb3ed54fdd6b808fa2d6ac224c17c3bb8
HoangTuGio <khangdeptrai198@gmail.com>
2025-04-16 00:42:56 +0700

    ContainsDuplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+        containsDuplicate123 c = new containsDuplicate123();
+        System.out.println(c.containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+}
+
+class containsDuplicate123 {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            }
+            map.put(num, 1);
+        }
+
+        return false;
+    }
+}
